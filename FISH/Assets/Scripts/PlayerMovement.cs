0cb6897d1886d161054948599fef8130f788cb2f
kevinleary <kevin.n.leary@gmail.com>
2015-12-29 13:26:04 -0500

    Version 0.1.1
    Relearning everything... going to look at old code... making assets
    still sucks

---
+++ FISH/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Rigidbody2D myRigidbody;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+        float movementSpeed;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool right = Input.GetKey(KeyCode.D);
+
+
+        if (right)
+        {
+
+
+        }
+    }
+}
